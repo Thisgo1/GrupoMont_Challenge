adc78ec1983aa56db2af8e38e116302ec007388d
Thiago Silva Ribeiro <thisgo.014@gmail.com>
2026-09-01 18:52:43 -0300

    chore(frontend): Adiciona novas dependências e configura o projeto para receber tailwind e shadcn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
-
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
+import path from 'path'   
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
